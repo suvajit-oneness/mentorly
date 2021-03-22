1c744f70df152d6dfbc5bc5b9936ae4e9456ccd3
Rajeev Gupta <rrpit9@gmail.com>
2021-03-22 17:59:42 +0530

    laravel previous version

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Get list of meetings.
+Route::get('/meetings', 'ZoomMeetingController@list');
+
+// Create meeting room using topic, agenda, start_time.
+Route::post('/meetings', 'ZoomMeetingController@create');
+
+// Get information of the meeting room by ID.
+Route::get('/meetings/{id}', 'ZoomMeetingController@get')->where('id', '[0-9]+');
+Route::patch('/meetings/{id}', 'ZoomMeetingController@update')->where('id', '[0-9]+');
+Route::delete('/meetings/{id}', 'ZoomMeetingController@delete')->where('id', '[0-9]+');
