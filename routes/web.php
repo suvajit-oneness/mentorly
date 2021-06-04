7801bb4f9d05ce0941ea83a720e1d91a86042c79
DevArpan7 <72331581+Arpan-Chanda@users.noreply.github.com>
2021-06-04 18:49:26 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,6 @@
 use Illuminate\Http\Request;
 
 
-// Route::get('messages', function () {
-//     return view('messages.pages.app');
-// });
-
-
-
-
-
-Route::get('command', function () {
-	/* php artisan migrate */
-    \Artisan::call('migrate:fresh --seed');
-    return "Done";
-});
 
 //user password reset routes
 Route::post('password/email','Auth\ForgotPasswordController@sendResetLinkEmail')->name('user.password.email');
@@ -76,69 +63,8 @@ Route::get('payment/bokking/success','StripePaymentController@thankyouPageToSHow
 Route::get('terms-and-condition','Site\WebsiteController@termsAndCondition')->name('terms.condition');
 Route::get('policy','Site\WebsiteController@policy')->name('policy');
 
-
+//get chats by conversation id
+Route::post('/get-messages-by-id', 'Site\MentorController@getMessagesById')->name('get.messages.by.id');
 
 // Cron Routes
 Route::get('create_teacher_slot','CronController@teacherSlot');
-
-
-// Route::get('stripe/{Id}/success','StripePaymentController@successTransaction')->name('stripe.success');
-
-// Auth::routes();
-// Route::group(['middleware' => ['auth','verified', 'userStatus']], function () {
-//  Route::get('profile', 'Site\ProfileController@index')->name('user.profile');
-//     Route::post('profile', 'Site\ProfileController@profileUpdate')->name('user.updateprofile');
-//     Route::get('checkout', 'Site\PackageController@index')->name('checkout');;
-//     //Route::get('upgrade/{id}', 'Site\PackageController@upgrade');
-//     Route::post('upgrade', 'Site\PackageController@upgrade')->name('adsUpgrade');
-//     Route::post('package', 'Site\PackageController@packageUpdate')->name('user.updatepackage');
-//     Route::get('logout', 'Site\ProfileController@logout')->name('user.logout');
-//     Route::get('my-ads/{type}','Site\ProfileController@fetchUserAds')->name('user.myads');
-
-//     Route::get('post-ads','Site\AdsController@craeteAds')->name('user.post.ad');
-//     Route::post('getCategoryFields', 'Site\AdsController@getCategoryFields')->name('user.customform.getCategoryFields');
-//     Route::post('ad-submit','Site\AdsController@storeAds')->name('adsubmit');
-
-//     Route::post('getCategoryFieldValues', 'Site\AdsController@getCategoryFieldValues')->name('ads.customform.getValues');
-//     Route::post('getRateValues', 'Site\AdsController@getRateValues')->name('ads.customform.getRateValues');
-//     Route::get('edit-ads/{id}','Site\AdsController@editAds');
-//     Route::post('updateads','Site\AdsController@updateAds')->name('updateads');
-//     Route::post('storeadmessage','Site\AdsController@storeAdMessage')->name('storeadmessage');
-//     Route::post('storereportabuse','Site\AdsController@storeReportAbuse')->name('storereportabuse');
-//     Route::get('update-free-package/{ad_id}/{id}','Site\PaymentController@updateFreePackage');
-// });
-
-    // Route::get('/',function(){
-    //     return redirect('/login');
-    // });
-/* Route::get('/', function()
-    {
-        return view('welcome');
-    }); */
-// Route::post ( '/stripe', 'Site\PaymentController@paymentProcess' )->name('paypost');
-// Route::post ( '/upgradepayment', 'Site\PaymentController@upgradeProcess' )->name('upgradepayment');
-
-//Route::view('/admin', 'admin.dashboard.index');
-
-/*=======================web-site============================*/
-
-// Route::get('/','Site\HomeController@index');
-// Route::get('ad-list','Site\AdsController@index');
-// Route::get('genre/{slug}','Site\ShowController@showsByGenre');
-// Route::get('list/{slug}','Site\ShowController@showsByCategory');
-// Route::get('details/{slug}','Site\ShowController@showDetails');
-
-// Route::get('ask-for-otp','Site\AuthController@askForOtp');
-// Route::post('mobile-no-submit','Site\AuthController@mobileNoSubmit');
-// Route::get('enter-otp/{id}','Site\AuthController@enterOtp');
-// Route::post('submit-otp','Site\AuthController@submitOtp');
-// Route::get('logout', 'Site\AuthController@logout');
-// Route::get('update-basic-data/{id}','Site\AuthController@updateBasicData');
-// Route::post('submit-basic-data','Site\AuthController@submitBasicData');
-// Route::get('subscribe','Site\PackageController@index');
-
-// Route::post('/pay-per-click-submit', 'Site\ShowController@payPerClickSubmit')->name('site.show.payPerClickSubmit');
-// Route::post('/store-package-information', 'Site\PackageController@storePackageInformation')->name('site.package.storepackageinfo');
-
-
-Route::post('/get-messages-by-id', 'Site\MentorController@getMessagesById')->name('get.messages.by.id');
