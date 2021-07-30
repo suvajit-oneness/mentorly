061397a6953357e5c3e6f1f47307d1bc15eff53a
Rajeev Gupta <rrpit9@gmail.com>
2021-07-30 11:52:04 +0530

    mentorly by Souvik

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -39,16 +39,16 @@
                 <label for="first_name">Name</label>
                 <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" value="{{ old('name') }}">
                 @error('first_name')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
                 <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password">
                 @error('password')
-                <span class="invalid-feedback" role="alert">
+                    <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
@@ -57,7 +57,7 @@
                 <label for="password_confirmation">Confirm Password</label>
                 <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" id="password_confirmation">
                 @error('password_confirmation')
-                <span class="invalid-feedback" role="alert">
+                    <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
@@ -67,7 +67,7 @@
             </div>
             <small class="text-muted">By clicking the 'Sign Up' button, you confirm that you accept our <br> Terms of use and Privacy Policy.</small>
         </form>
-    <!-- <div class="border-top card-body text-center">Have an account? <a href="{{ route('admin.login') }}">Log In</a></div> -->
+        <!-- <div class="border-top card-body text-center">Have an account? <a href="{{ route('admin.login') }}">Log In</a></div> -->
         </form>
     </div>
 </section>
@@ -78,85 +78,3 @@
 <script src="{{ asset('backend/js/plugins/pace.min.js') }}"></script>
 </body>
 </html>
-
-
-
-
-
-
-
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
-    <title>Register - {{ config('app.name') }}</title>
-</head>
-<body>
-<section class="material-half-bg">
-    <div class="cover"></div>
-</section>
-    <section class="register-content">
-        <div class="container">
-            <div class="col-md-6 mx-auto">
-                <div class="card">
-                    <header class="card-header">
-                        <h4 class="card-title mt-2"><i class="fa fa-lg fa-fw fa-user"></i>Sign up</h4>
-                    </header>
-                    <article class="card-body">
-                        <form action="{{ route('admin.register') }}" method="POST" role="form">
-                            @csrf
-                            <div class="form-group">
-                                <label for="email">E-Mail Address</label>
-                                <input type="email" class="form-control disabled" value="{{ $email }}" disabled>
-                                <input id="email" type="hidden" class="form-control disabled" name="email" value="{{ $email }}">
-                            </div>
-                            <div class="form-group">
-                                <label for="first_name">Name</label>
-                                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" value="{{ old('name') }}">
-                                @error('first_name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password">
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="password_confirmation">Confirm Password</label>
-                                <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" id="password_confirmation">
-                                @error('password_confirmation')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-success btn-block"> Sign Up </button>
-                            </div>
-                            <small class="text-muted">By clicking the 'Sign Up' button, you confirm that you accept our <br> Terms of use and Privacy Policy.</small>
-                        </form>
-                    </article>
-                    <div class="border-top card-body text-center">Have an account? <a href="{{ route('admin.login') }}">Log In</a></div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <script src="{{ asset('backend/js/jquery-3.2.1.min.js') }}"></script>
-    <script src="{{ asset('backend/js/popper.min.js') }}"></script>
-    <script src="{{ asset('backend/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('backend/js/main.js') }}"></script>
-    <script src="{{ asset('backend/js/plugins/pace.min.js') }}"></script>
-</body>
-</html> -->
