b143c496f8dfa17b67b86668fba64c9847d7106f
Souvik De <techsj04@gmail.com>
2021-08-04 12:59:31 +0530

    changes

--- resources/views/stripe/thankyou.blade.php
+++ resources/views/stripe/thankyou.blade.php
@@ -7,9 +7,8 @@
 	<h1>Your Payment was successFull</h1>
 	<br>
 	<h3>Note : Please Save the Below Details for future Referance</h3><br>
-	<span>Booking Id : <strong>{{$stripe->id}}</strong></span><br>
 	<span>Transaction Id : <strong>{{$stripe->transactionId}}</strong></span><br>
-	<span>Amount Charged : Rs.<strong>{{$stripe->amount}} </strong></span>
+	<span>Amount Charged : Rs.<strong>{{$stripe->amount/100}} </strong></span>
 
 	<br><br><br><br>
 	<a href="{{url('/')}}">Click here to go home</a>
